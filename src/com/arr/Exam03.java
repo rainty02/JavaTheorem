560cdfa9cdbfe981d3532da802371471b1a5c400
rainty02 <rainty02@gmail.com>
2021-05-27 18:24:43 +0900

    배열 행 이동

---
+++ src/com/arr/Exam03.java
@@ -0,0 +1,38 @@
+package com.arr;
+
+public class Exam03 {
+
+	public static void lineChange (int[][] arr) {
+		int[] temp = new int[1];
+		for(int inx = 0; inx < arr.length - 1; inx++) {
+			temp = arr[inx];
+			arr[inx] = arr[inx+1];
+			arr[inx+1] = temp;
+		}
+		for(int inx = 0; inx < arr.length - 1; inx++) {
+			temp = arr[inx];
+			arr[inx] = arr[inx+1];
+			arr[inx+1] = temp;
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		int[][] arr = {
+				{1,2,3},
+				{4,5,6},
+				{7,8,9}
+		};
+
+		Exam03 ex = new Exam03();
+		ex.lineChange(arr);
+		
+		for(int inx = 0; inx <arr.length; inx++) {
+			for(int inx2 = 0; inx2 <arr.length; inx2++) {
+				System.out.print(arr[inx][inx2]);
+			}
+			System.out.println();
+		}		
+	}
+}
