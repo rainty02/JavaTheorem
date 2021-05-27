948b4e73e33dc4744a328f54eaced083fa14cfe8
rainty02 <rainty02@gmail.com>
2021-05-27 19:07:27 +0900

    배열 과제1
    메소드 코드수정

--- src/com/arr/Exam01.java
+++ src/com/arr/Exam01.java
@@ -5,27 +5,23 @@ import java.util.Scanner;
 public class Exam01 {
 
 	public static int miniValue(int[] arr) {
-		for(int i = 0; i < arr.length - 1; i++) {
-			if(arr[i] < arr[i+1]) {
-				int temp = 0;
-				temp = arr[i];
-				arr[i] = arr[i+1];
-				arr[i+1] = temp;				
+		int temp = arr[0];
+		for(int i = 1; i < arr.length; i++) {
+			if(temp > arr[i]) {
+				temp = arr[i];				
 			}
 		}
-		return arr[arr.length-1];
+		return temp;
 	}
 	
 	public static int maxValue(int[] arr) {
-		for(int i = 0; i < arr.length - 1; i++) {
-			if(arr[i] > arr[i+1]) {
-				int temp = 0;
+		int temp = arr[0];
+		for(int i = 1; i < arr.length; i++) {
+			if(temp < arr[i]) {
 				temp = arr[i];
-				arr[i] = arr[i+1];
-				arr[i+1] = temp;				
 			}
 		}
-		return arr[arr.length-1];
+		return temp;
 	}
 		
 	public static void main(String[] args) {
