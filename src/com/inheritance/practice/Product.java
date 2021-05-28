ab5bd5317baa7a996d3bba38312f1ea586f94b36
rainty02 <rainty02@gmail.com>
2021-05-28 15:30:04 +0900

    상속 - 다형성

---
+++ src/com/inheritance/practice/Product.java
@@ -0,0 +1,12 @@
+package com.inheritance.practice;
+
+public class Product {
+	
+	final int price;
+	int bonusPoint;
+	
+	public Product(int price) {
+		this.price = price;
+		bonusPoint = (int)(price/10.0);
+	}
+}
