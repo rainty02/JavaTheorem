399c8f6a0f9440df04ec4670e7bd070d02156699
rainty02 <rainty02@gmail.com>
2021-05-31 18:49:35 +0900

    iterator 사용법

--- src/com/collection/practice/IteratorTest.java
+++ src/com/collection/practice/IteratorTest.java
@@ -16,22 +16,25 @@ public class IteratorTest {
 		list.add("Second");
 		list.add("Third");
 		list.add("Forth");
-	
+		
+		// length나 카운트용 변수 대신 size() 을 사용
 		for(int i=0; i<list.size(); i++) {
 			System.out.println(list.get(i));
 		}
 		
 		System.out.println("------------------");
 		
+		// for each문
 		for(String str : list) {
 			System.out.println(str);
 		}
 	
 		System.out.println("------------------");
 		
+		// iterator 사용
 		Iterator<String> itr = list.iterator();
-		while(itr.hasNext()) {
-			String currentStr = itr.next();
+		while(itr.hasNext()) { // 다음 값이 있다면 true, 없다면 false
+			String currentStr = itr.next(); // 다음 값을 변수에 대입
 			System.out.println(currentStr);
 		}
 	}
