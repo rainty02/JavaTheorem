27c2f09b2e4bb86b85ef875b288c1f24581f7b80
rainty02 <rainty02@gmail.com>
2021-06-01 16:55:31 +0900

    주석 추가

--- src/com/abstract_interface/exam/Calculator.java
+++ src/com/abstract_interface/exam/Calculator.java
@@ -1,7 +1,7 @@
 package com.abstract_interface.exam;
 
 interface Calculator { // 인터페이스
-	long add(long n1, long n2);
+	long add(long n1, long n2); // public abstract 생략
 	long substract(long n1, long n2 );
 	long multiply(long n1, long n2 );
 	double divide(double n1, double n2 );	
--- src/com/abstract_interface/exam/CalculatorAbs.java
+++ src/com/abstract_interface/exam/CalculatorAbs.java
@@ -8,6 +8,6 @@ public abstract class CalculatorAbs implements Calculator {
 	public long substract(long n1, long n2) {
 		return n1 - n2;
 	}
-	public abstract long multiply(long n1, long n2); // 추상클래스
-	public abstract double divide(double n1, double n2);
+//	public abstract long multiply(long n1, long n2); // 추상클래스
+//	public abstract double divide(double n1, double n2); // 인터페이스를 상속받아 추상 메소드는 없어도 됨
 }
