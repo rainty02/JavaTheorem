5d303a1807f0d43d810912e7300b97c4b804e36c
rainty02 <rainty02@gmail.com>
2021-06-11 16:03:46 +0900

    sql 연습3

--- SQL/20210611_sql_practice3.sql
+++ SQL/20210611_sql_practice3.sql
@@ -61,10 +61,15 @@ group by job
 ;
 
 -- 26. 관리자 수를 출력하시오.
-select mgr, count(*)
+select mgr, count(mgr)
 from emp
 group by mgr
 ;
+--수정
+select count(distinct mgr)
+from emp
+;
+
 
 -- 27. 급여 최고액, 급여 최저액의 차액을 출력하시오.
 select max(sal) - min(sal)
@@ -84,9 +89,10 @@ order by minsal desc
 
 -- 29. 각 부서에 대해 부서번호, 사원 수, 부서 내의 모든 사원의 평균 급여를 출력하시오.
 -- 평균 급여는 소수점 둘째 자리로 반올림 하시오.
-select deptno, count(*) as "사원의 수", round(avg(sal)) as "평균 급여"
+select deptno, count(*) as "사원의 수", round(avg(sal), 2) as "평균 급여"
 from emp
 group by deptno
+order by deptno
 ;
 
 -- 30. 각 부서에 대해 부서번호 이름, 지역 명, 사원 수, 부서내의 모든 사원의 평균 급여를 출력하시오.
