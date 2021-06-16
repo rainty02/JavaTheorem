c97e052dfc44b4451555a5831b70603dd9e67e3a
rainty02 <rainty02@gmail.com>
2021-06-16 15:35:16 +0900

    dll 생성

--- SQL/20210615_sql_create_ddl.sql
+++ SQL/20210615_sql_create_ddl.sql
@@ -1,33 +1,40 @@
 -- 20210615 DDL 생성
 
 create table phoneInfo_basic(
-    idx number(6) constraint phoneInfo_basic_idx_pk primary key,
-    f_name varchar2(20) constraint phoneInfo_basic_f_name_nn not null,
-    f_tel varchar2(20) constraint phoneInfo_basic_f_tel_nn not null,
-    f_email varchar2(20),
-    f_address varchar2(20),
-    f_redate date default sysdate
+    idx number(6) constraint PI_B_idx_pk primary key,
+    fr_name varchar2(20) not null,
+    fr_tel varchar2(20) not null,
+    fr_email varchar2(20),
+    fr_address varchar2(20),
+    fr_redate date default sysdate
 );
 
 create table phoneInfo_univ(
-    idx number(6) constraint phoneInfo_univ_idx_pk primary key,
-    u_major varchar2(20) default 'N'
-                         constraint phoneInfo_univ__u_major_mnn not null,
-    u_year number(1) default '1' 
-                     constraint phoneInfo_univ_u_year_ck check (u_year > 0 and u_year <5)
-                     constraint phoneInfo_univ_u_year_ynn not null,
-    f_ref number(6) constraint phoneInfo_univ_f_ref_rnn not null
-                    constraint phoneInfo_univ_f_ref_fk references phoneInfo_basic(idx)
+    idx number(6) constraint PI_U_idx_pk primary key,
+    fr_u_major varchar2(20) default 'N' not null,
+    fr_u_year number(1) default '1' not null
+                        constraint PI_U_fr_u_year_ck check (fr_u_year between 1 and 4),
+    fr_ref number(6) not null
+                     constraint PI_U_fr_ref_fk references phoneInfo_basic(idx)
 );
 
 create table phoneInfo_com(
     idx number(6),
-    c_company varchar(20) default 'N' constraint phoneInfo_com_c_company_nn not null,
-    f_ref number(6) constraint phoneInfo_com_f_ref_nn not null,
-    constraint phoneInfo_com_idx_pk primary key(idx),
-    constraint phoneInfo_com_c_company foreign key(f_ref) references phoneInfo_basic(idx)
+    fr_c_company varchar(20) default 'N' not null,
+    fr_ref number(6) not null,
+    constraint PI_C_idx_pk primary key(idx),
+    constraint PI_C_fr_c_company foreign key(fr_ref) references phoneInfo_basic(idx)
 );
+drop table phoneInfo_basic;
 
 desc phoneInfo_basic;
 desc phoneInfo_univ;
 desc phoneInfo_com;
+
+
+
+
+
+
+
+
