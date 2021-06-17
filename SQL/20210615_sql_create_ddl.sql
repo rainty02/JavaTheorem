d58b870ab63a48b83bfb406925e2c516d5f781fe
rainty02 <rainty02@gmail.com>
2021-06-17 18:09:57 +0900

    ddl 생성

--- SQL/20210615_sql_create_ddl.sql
+++ SQL/20210615_sql_create_ddl.sql
@@ -1,40 +1,34 @@
 -- 20210615 DDL 생성
 
-create table phoneInfo_basic(
-    idx number(6) constraint PI_B_idx_pk primary key,
-    fr_name varchar2(20) not null,
-    fr_tel varchar2(20) not null,
-    fr_email varchar2(20),
+create table phoneInfo_basic (
+    idx number(6) constraint pi_basic_idx_PK primary key,
+    fr_name VARCHAR2(20) not null,
+    fr_phonenumber varchar2(20) not null,
+    fr_email varchar2(20) ,
     fr_address varchar2(20),
-    fr_redate date default sysdate
+    fr_regdate date default sysdate
 );
 
-create table phoneInfo_univ(
-    idx number(6) constraint PI_U_idx_pk primary key,
-    fr_u_major varchar2(20) default 'N' not null,
-    fr_u_year number(1) default '1' not null
-                        constraint PI_U_fr_u_year_ck check (fr_u_year between 1 and 4),
-    fr_ref number(6) not null
-                     constraint PI_U_fr_ref_fk references phoneInfo_basic(idx)
-);
-
-create table phoneInfo_com(
+create table phoneinfo_univ (
     idx number(6),
-    fr_c_company varchar(20) default 'N' not null,
+    fr_u_major VARCHAR2(20) default 'N' not null,
+    fr_u_year number(1) default 1  not null, --check (fr_u_year between 1 and 4),
     fr_ref number(6) not null,
-    constraint PI_C_idx_pk primary key(idx),
-    constraint PI_C_fr_c_company foreign key(fr_ref) references phoneInfo_basic(idx)
+    constraint pi_univ_idx_PK primary key (idx),
+    constraint chk check (fr_u_year between 1 and 4), -- 테이블 레벨에서 check  제약 설정 
+    constraint pi_univ_ref_FK FOREIGN KEY (fr_ref) REFERENCES phoneInfo_basic (idx)
 );
-drop table phoneInfo_basic;
+
+create table phoneinfo_com (
+    idx number(6) constraint pi_com_idx_PK primary key,
+    fr_c_company VARCHAR2(20) default 'N' not null,
+    fr_ref number(6) not null constraint pi_com_ref_FK REFERENCES phoneinfo_basic (idx)
+);
+
+drop table phoneInfo_BASIC;
 
 desc phoneInfo_basic;
 desc phoneInfo_univ;
 desc phoneInfo_com;
-
-
-
-
-
-
 
 
