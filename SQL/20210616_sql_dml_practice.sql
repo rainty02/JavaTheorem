1ea3b14be1158d56ff0f84ee39f3fa6b9496bf27
rainty02 <rainty02@gmail.com>
2021-06-17 18:09:50 +0900

    dml 연습

--- SQL/20210616_sql_dml_practice.sql
+++ SQL/20210616_sql_dml_practice.sql
@@ -107,11 +107,126 @@ UPDATE PHONEINFO_COM SET FR_C_COMPANY = '맨시티' WHERE IDX = 3;
 DELETE FROM PHONEINFO_COM WHERE IDX = 3;
 DELETE FROM PHONEINFO_COM WHERE IDX = (SELECT IDX FROM PHONEINFO_COM WHERE FR_C_COMPANY = NULL);
 
+--------------------------------------------------------
 
+-- BASIC INSERT : CREATE
+DESC phoneinfo_basic;
+insert into phoneinfo_basic
+values (1, 'KING', '010-0000-0000', 'king@gmail.com', 'KOREA', sysdate)
+;
+insert into phoneinfo_basic (idx, fr_name, fr_phonenumber)
+values (2, 'SCOTT', '010-9999-9999')
+;
+
+-- SELECT : READ
+select * from phoneinfo_basic;
+select * from phoneinfo_basic where idx=2;
+
+-- UPDATE : UPDATE
+-- SCOTT의 이메일을 업데이트, 주소도 업데이트
+-- SCOTT 의 idx -> 2
+ update phoneinfo_basic
+ set fr_email='scott@naver.com', fr_address='서울'
+ where idx=2 --fr_name='SCOTT'
+;
+
+-- DELETE : DELETE
+delete from phoneinfo_basic
+where idx=1
+;
+
+
+-- UNIV INSERT : CREATE 데이터 생성
+-- 대학친구의 정보를 입력
+-- 1 basic 정보 입력
+insert into phoneinfo_basic
+values (PI_IDX_PK.nextval, 'SON', '010-1111-1111', 'son@gmail.com', 'KOREA', sysdate)
+;
+-- 2. univ 정보 입력
+insert into phoneinfo_univ
+values (PI_U_IDX_PK.nextval, 'COMPUTER', 4, PI_IDX_PK.currval)
+;
+
+-- SELECT : READ   데이터 검색
+select pb.idx, pu.fr_ref, pu.idx, fr_name, pu.fr_u_major, pu.fr_u_year
+from phoneinfo_basic pb , phoneinfo_univ pu
+where pb.idx=pu.fr_ref
+;
+
+-- UPDATE : UPDATE 데이터 수정
+-- 전공과 학년을 수정, idx 또는 외래키
+update phoneinfo_univ
+set fr_u_major='KOR', fr_u_year=1
+where idx=1
+;
+
+-- DELETE : DELETE 데이터 삭제
+delete from phoneinfo_univ
+where idx=1
+;
+delete from phoneinfo_basic
+where idx=3
+;
+
+
+-- COM INSERT : CREATE
+insert into phoneinfo_basic
+values (pi_idx_pk.nextval, 'PARK', '010-7777-7777', 'park@gmail.com', 'LONDON', sysdate)
+;
+insert into phoneinfo_com values (PI_C_IDX_PK.nextval, 'NAVER', pi_idx_pk.currval);
+
+-- SELECT : READ
+select fr_name, pb.fr_phonenumber, pb.fr_email, pb.fr_address, pc.fr_c_company
+from phoneinfo_basic pb , phoneinfo_com pc
+where pb.idx=pc.fr_ref
+;
+
+-- UPDATE : UPDATE
+-- 회사정보를 수정
+update phoneinfo_com
+set fr_c_company='GOOGLE'
+where idx=1
+;
+
+-- DELETE : DELETE
+delete from phoneinfo_com
+where idx=1
+;
+delete from phoneinfo_basic
+where idx=4
+;
 
 -- 전체 친구 정보
 select *
-from phoneinfo_basic pb, phoneinfo_univ pu, phoneinfo_com pc
-where pb.idx = pu.fr_ref and pb.idx = pc.fr_ref(+)
+from phoneinfo_basic pb , phoneinfo_univ pu, phoneinfo_com pc
+where pb.idx=pu.fr_ref(+) and pb.idx=pc.fr_ref(+)
 ;
+
+
+
+-- 대학친구, 회사 친구 테이블 -> 기본키(대리키) -> sequence 생성 -> insert 개선
+
+
+-- SEQUENCE : 번호 재생기
+create sequence pi_idx_pk;
+create sequence pi_u_idx_pk start with 1 increment by 1;
+create sequence pi_c_idx_pk start with 1 increment by 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
