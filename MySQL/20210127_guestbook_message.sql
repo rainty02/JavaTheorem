081aee36c63553d62d55407e2debda617abb4398
rainty02 <rainty02@gmail.com>
2021-07-27 11:29:15 +0900

    방명록 연습

--- MySQL/20210127_guestbook_message.sql
+++ MySQL/20210127_guestbook_message.sql
@@ -8,8 +8,24 @@ create table project.guestbook_message(
 
 -- select
 -- 최근 게시된 순서대로
-select * from project.guestbook_message order by ragdate desc;
+select * from project.guestbook_message order by regdate desc;
 -- limit index, count / index부터 count개씩 (페이징 처리)
 select * from project.guestbook_message limit 0, 3;
+
+-- insert
+insert into project.guestbook_message (guestname, password, message)
+values ('?', '?', '?');
+
+-- update
+update project.guestbook_message
+set guestname='scott', password='0000', message='안녕하세요'
+where messageid = 1;
+
+-- delete
+delete from project.guestbook_message where messageid = 1;
+
+
+
+
 
 
