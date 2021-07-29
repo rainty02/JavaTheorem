e414e0e341a18ef78a62ece294abb20c0edeff2a
rainty02 <rainty02@gmail.com>
2021-07-29 18:22:45 +0900

    컨트롤러 추가

---
+++ jsp/Login/src/controller/FrontController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FrontController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+	
+	private void doProcess(HttpServletRequest request, HttpServletResponse response) {
+		
+		// 1. 사용자 요청 분석
+		String commandUri = request.getRequestURI();
+		
+	}
+	
+	
+
+}
