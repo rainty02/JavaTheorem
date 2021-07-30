3b11ff9194ec01a2adf898b20757183ede5e728b
rainty02 <rainty02@gmail.com>
2021-07-30 13:20:23 +0900

    MVC 패턴 추가

--- jsp/Login/src/controller/FrontController.java
+++ jsp/Login/src/controller/FrontController.java
@@ -23,6 +23,23 @@ public class FrontController extends HttpServlet {
 		
 		// 1. 사용자 요청 분석
 		String commandUri = request.getRequestURI();
+		// 입력된 Uri 중 컨텍스트 경로가 0번째 위치하면
+		if(commandUri.indexOf(request.getContextPath()) == 0 ) {
+			// 입력한 Uri 경로 중 컨텍스트 경로의 길이만큼 지운다 : /mvc/guest/list.do - > /mvc을 지움
+			commandUri = commandUri.substring(request.getContextPath().length());
+		}
+		
+		// 결과 데이터
+		String resultObj = null;
+		// 결과 페이지
+		String viewPage = null;
+		
+		// 2. 요청 처리
+		if(commandUri.equals("/idcheck.do")) {
+			// 처리할 서비스 메소드 실행
+			resultObj = "아이디를 체크합니다";
+			viewPage = "/";
+		}
 		
 	}
 	
